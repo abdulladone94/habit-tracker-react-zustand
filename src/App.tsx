7146b9be6967b116aef9b9d54e79e15363eaec55
abdulladone94 <abdullaf@booleanlabs.biz>
2024-10-07 14:03:15 +0530

    add habit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Box, Container, Typography } from '@mui/material';
 import './App.css';
+import useHabitStore from './store/store';
+import AddHabitForm from './components/add-habit-form';
 
 function App() {
+  const store = useHabitStore();
+  console.log(store);
   return (
-    <div>
-      <h1>hello wor</h1>
-    </div>
+    <Container>
+      <Box>
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Habit Tracker
+        </Typography>
+        {/* Form  */}
+        <AddHabitForm />
+        {/* List  */}
+        {/* Stats  */}
+      </Box>
+    </Container>
   );
 }
 
